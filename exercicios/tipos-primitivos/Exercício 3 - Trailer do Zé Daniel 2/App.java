bf1e7be697af3688159200d64958a256c75f0e91
Allan Nogueira <allannog.dev@gmail.com>
2026-03-09 07:33:43 -0300

    Exercícios 1, 2 e 3

---
+++ exercicios/tipos-primitivos/Exercício 3 - Trailer do Zé Daniel 2/App.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+        int horas = 3;
+
+        System.out.print("Quantidade de lanches consumidos na primeira hora: ");
+        int primeiraHora = teclado.nextInt();
+
+        System.out.print("Quantidade de lanches consumidos na segunda hora: ");
+        int segundaHora = teclado.nextInt();
+
+        System.out.print("Quantidade de lanches consumidos na terceira hora: ");
+        int terceiraHora = teclado.nextInt();
+
+        int soma = primeiraHora + segundaHora + terceiraHora;
+        double media = (double) soma / horas;
+
+        System.out.println("Total: " + soma);
+        System.out.println("Média: " + media);
+
+        teclado.close();
+    }
+}
